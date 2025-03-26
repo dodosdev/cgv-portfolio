e5b273d341acdd95bacfe73cbc7293f3f3dbe8f3
dojin <djsong33@gmail.com>
2025-03-26 15:34:33 +0900

    update

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import AppCgv from './components/cgv_layout/AppCgv.jsx'
+
+
+
+export default function App() {
+
+  return (
+    <div className="App">
+      <AppCgv />
+    </div>
+    
+  );
+}
