d305f264b044a5373560e1472072c44f2f0df104
dojin <djsong33@gmail.com>
2025-03-26 15:37:36 +0900

    특별관 수정

--- src/components/cgv_layout/content/Special.jsx
+++ src/components/cgv_layout/content/Special.jsx
@@ -41,12 +41,6 @@ export default function Special(){
                         )}
                     </ul>
                 </div>
-                const A = 1
-                const B = 3
-
-                const result = (A==2) && (B==3)
-
-
             </div>
         </section>
     );
